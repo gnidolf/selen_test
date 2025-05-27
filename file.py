75c5d3ec313fbbee6f06b5ad69d168404a161c24
Nikita Shabaev <nshab@tst.local>
2025-05-27 12:32:50 +0300

    save session jscode

--- file.py
+++ file.py
@@ -3,4 +3,17 @@ import json
 
 from selenium import webdriver
 
-session = 'whatsapp_session.json'
+session_file = 'whatsapp_session.json'
+
+def save_session(driver):
+    cookies = driver.get_cookies()
+
+    local_storage = driver.execute_script(
+        "let ls = {};"
+        "for(let i = 0; i < localStorage.lenght; i++){"
+        "let key = localStorage.key(i);"
+        "ls[key] = localStorage.getIteim(key);"
+        "} return ls" 
+    )
+
+    with open
